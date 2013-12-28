52f319496db88fb2efd4116c3001ca82e202ceb6
Peter Donald <peter@realityforge.org>
2013-12-28 20:24:15 +1100

    Reimplement toString using a cached json factory

--- src/main/java/org/realityforge/jeo/geojson/GjElement.java
+++ src/main/java/org/realityforge/jeo/geojson/GjElement.java
@@ -2,6 +2,7 @@ package org.realityforge.jeo.geojson;
 
 import java.io.StringWriter;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -9,11 +10,14 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonGeneratorFactory;
 import org.geolatte.geom.Envelope;
 import org.geolatte.geom.crs.CrsId;
 
 public abstract class GjElement
 {
+  private static final JsonGeneratorFactory FACTORY = Json.createGeneratorFactory( new HashMap<String, Object>() );
+
   @Nonnull
   private final Map<String, JsonValue> _additionalProperties;
   @Nullable
@@ -69,12 +73,26 @@ public abstract class GjElement
   }
 
   @Override
-  public String toString()
+  public final String toString()
   {
+    /*
+     * We use a cached factory here, otherwise creating the generator
+     * becomes the bottleneck. Here are the timings for different
+     * implementations of this method.
+     *
+     * 35167.862 ns - creating factory every time
+     * 2483.863 ns - reusing static factory
+     */
+
     final StringWriter writer = new StringWriter();
-    final JsonGenerator g = Json.createGenerator( writer );
+    final JsonGenerator g = FACTORY.createGenerator( writer );
+    write( g );
+    return writer.toString();
+  }
+
+  public final void write( final JsonGenerator g )
+  {
     new GeoJsonWriter().write( g, this );
     g.close();
-    return writer.toString();
   }
 }
